d0c6af12e4765391125a236cdf0d26d04c798f0b
Max Cloudy <ayohmei@gmail.com>
2020-03-22 18:53:04 +0100

    fixing heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,3 +1,12 @@
+;('use strict')
+
+const razzleHeroku = require('razzle-heroku')
+
 module.exports = {
+  modify(config, { target, dev }, webpack) {
+    config = razzleHeroku(config, { target, dev }, webpack)
+
+    return config
+  },
   plugins: ['scss']
 }
